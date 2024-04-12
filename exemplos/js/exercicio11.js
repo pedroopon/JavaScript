8ac910ee0126841580cb395b88e80c74756f26af
Pedro Xavier <xppontes@gmail.com>
2024-04-12 01:15:16 -0300

    exercicios

---
+++ exemplos/js/exercicio11.js
@@ -0,0 +1,5 @@
+function alterarParagrafo(){
+    document.getElementById('paragrafo01').innerHTML = "Esse é o primeiro paragrafo"
+    document.getElementById('paragrafo02').innerHTML = "Esse é o primeiro paragrafo"
+    document.getElementsByTagName('body')[0].style.backgroundColor = "#1aa7a7"
+}
